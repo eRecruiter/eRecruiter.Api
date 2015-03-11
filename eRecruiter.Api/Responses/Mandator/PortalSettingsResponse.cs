a379b3b9b1ea5935abf4484f6ddad969519fd3f8
Adrian Fürschuß <adrian.fuerschuss@epunkt.net>
2015-03-11 13:17:17 +0100

    #8: Modify settings for applicant personal info.
    Add new settings for applicant personal info. Change type of existing
    info settings to DisplayType. Order properties by occurence on portal.

--- eRecruiter.Api/Responses/Mandator/PortalSettingsResponse.cs
+++ eRecruiter.Api/Responses/Mandator/PortalSettingsResponse.cs
@@ -1,4 +1,6 @@
 ﻿
+using eRecruiter.Api.Parameters;
+
 namespace eRecruiter.Api.Responses
 {
     public class PortalSettingsResponse
@@ -6,27 +8,88 @@ namespace eRecruiter.Api.Responses
         // please note that the properties are sorted just like in the eR admin area
         // don't break this!
 
+        #region General settings
+
         public bool ForceSsl { get; set; }
         public bool EnableSelfServiceZone { get; set; }
         public bool AllowApplicationWithoutJob { get; set; }
         public string DefaultCulture { get; set; }
 
+        #endregion General settings
+
         public bool SendRegistrationEmail { get; set; }
         public bool SendApplicationEmail { get; set; }
 
-        public DisplayType AskForBirthdate { get; set; }
+        #region Applicanta data
+
+        /// <summary>
+        /// Specifies if or how the input field of an applicants's <see cref="ApplicantParameter.Gender"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
         public DisplayType AskForGender { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input fields of an applicants's <see cref="ApplicantParameter.TitleAfterName"/> or <see cref="ApplicantParameter.TitleAfterName"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForTitles { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input field of an applicants's <see cref="ApplicantParameter.Email"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForEmail { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input field of an applicants's <see cref="ApplicantParameter.Phone"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForPhoneNumber { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input field of an applicants's <see cref="ApplicantParameter.BirthDate"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForBirthdate { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input fields of an applicants's <see cref="ApplicantParameter.Street"/>, <see cref="ApplicantParameter.ZipCode"/> and <see cref="ApplicantParameter.City"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForAddress { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input field of an applicants's <see cref="ApplicantParameter.Country"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForCountry { get; set; }
+
+        /// <summary>
+        /// Specifies the default value of an applicant's <see cref="ApplicantParameter.Country"/> property.
+        /// </summary>
         public string DefaultCountry { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input field of an applicants's <see cref="ApplicantParameter.Citizenship"/> should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForCitizenship { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the input field of a coverletter should be displayed on the register page or the job application page.
+        /// </summary>
         public DisplayType AskForCoverLetter { get; set; }
-        public bool AskForTitles { get; set; }
-        public bool AskForCitizenship { get; set; }
-        public bool AskForEmailConfirmation { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the upload control of an applicants's CV (<see cref="ApplicantCvParameter"/>) should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForCv { get; set; }
+
+        /// <summary>
+        /// Specifies if or how the upload control of an applicants's picture (<see cref="ApplicantPhotoParameter"/>) should be displayed in the Self-Service-Zone.
+        /// </summary>
+        public DisplayType AskForPhoto { get; set; }
+
+        /// <summary>
+        /// Specifies wether the upload control of an applicants's documents (<see cref="ApplicantDocumentParameter"/>) should be displayed in the Self-Service-Zone or not.
+        /// </summary>
+        public bool AskForDocuments { get; set; }
+
         public bool AskForReferrer { get; set; }
         public bool AskForLocation { get; set; }
-        public bool AskForRegion { get; set; }
-        public DisplayType AskForCv { get; set; }
-        public DisplayType AskForPhoto { get; set; }
-        public bool AskForDocuments { get; set; }
+        public bool AskForRegion { get; set; }    
         public bool AskForNewsletter { get; set; }
         public bool AskForMatchingJobsMail { get; set; }
         public bool AskForQuitReason { get; set; }
@@ -52,6 +115,17 @@ namespace eRecruiter.Api.Responses
         public bool AskForTags { get; set; }
         public bool AskForCareerLevel { get; set; }
         public bool AskForJobProfiles { get; set; }
+
+        #region Applicanta data on register page
+
+        /// <summary>
+        /// Specifies if a second input field for confirmation of an applicant's <see cref="ApplicantParameter.Email"/> should be displayed on the register page.
+        /// </summary>
+        public bool AskForEmailConfirmation { get; set; }
+
+        #endregion Applicanta data on register page
+
+        #endregion Applicanta data
 
         public bool DisplayLocationInJobListing { get; set; }
         public bool DisplayDateInJobListing { get; set; }
