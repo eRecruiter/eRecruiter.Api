d4ee5b63e821f6684ccf532d1417013494ffb169
Bernhard Wolkerstorfer <wolkerstorfer@erecruiter.net>
2015-06-23 11:25:01 +0200

    #10 extended JobPublicationResponse with multi-posting partner information

--- eRecruiter.Api/Responses/Job/JobPublicationResponse.cs
+++ eRecruiter.Api/Responses/Job/JobPublicationResponse.cs
@@ -1,15 +1,23 @@
-﻿
-namespace eRecruiter.Api.Responses
+﻿namespace eRecruiter.Api.Responses
 {
     public class JobPublicationResponse
     {
         public string Name { get; set; }
+        public string Account { get; set; }
+        public string Channel { get; set; }
         public JobPublicationType Type { get; set; }
+        public JobPublicationPartner Partner { get; set; }
 
         public enum JobPublicationType
         {
             Portal,
-            Channel
+            Channel,
+            MultiPostChannel
+        }
+
+        public enum JobPublicationPartner
+        {
+            Persy
         }
     }
 }
