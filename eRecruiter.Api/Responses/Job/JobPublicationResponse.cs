0ea947de1bdeec317270311ba690c8f2626457ab
Bernhard Wolkerstorfer <wolkerstorfer@erecruiter.net>
2015-06-23 16:28:48 +0200

    #10 increased flexibility for multi-post partner selection on API endpoint

--- eRecruiter.Api/Responses/Job/JobPublicationResponse.cs
+++ eRecruiter.Api/Responses/Job/JobPublicationResponse.cs
@@ -7,22 +7,16 @@ namespace eRecruiter.Api.Responses
         public string Name { get; set; }
         public string Account { get; set; }
         public string Channel { get; set; }
+        public string Partner { get; set; }
         public DateTime PublicationDate { get; set; }
         public DateTime? ExpirationDate { get; set; }
-
         public JobPublicationType Type { get; set; }
-        public JobPublicationPartner Partner { get; set; }
 
         public enum JobPublicationType
         {
             Portal,
             Channel,
             MultiPostChannel
-        }
-
-        public enum JobPublicationPartner
-        {
-            Persy
         }
     }
 }
