c747257e329fccefcd4b5cceb36f93840a0da116
Bernhard Wolkerstorfer <wolkerstorfer@erecruiter.net>
2015-06-24 11:37:43 +0200

    #10 fixed data type for channel

--- eRecruiter.Api/Responses/Job/JobPublicationResponse.cs
+++ eRecruiter.Api/Responses/Job/JobPublicationResponse.cs
@@ -5,9 +5,9 @@ namespace eRecruiter.Api.Responses
     public class JobPublicationResponse
     {
         public string Name { get; set; }
-        public string Account { get; set; }
-        public string Channel { get; set; }
         public string Partner { get; set; }
+        public string Account { get; set; }
+        public int? Channel { get; set; }
         public DateTime PublicationDate { get; set; }
         public DateTime? ExpirationDate { get; set; }
         public JobPublicationType Type { get; set; }
