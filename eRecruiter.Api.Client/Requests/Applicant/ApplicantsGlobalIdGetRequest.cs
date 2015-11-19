060a99cbf9d3031e5b3a98fd3686308b0feaef89
Christoph Kopf <christoph.kopf@erecruiter.net>
2015-11-19 17:36:28 +0100

    Add GlobalID Request to Api Client

---
+++ eRecruiter.Api.Client/Requests/Applicant/ApplicantsGlobalIdGetRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using eRecruiter.Api.Responses;
+
+namespace eRecruiter.Api.Client.Requests
+{
+    public class ApplicantsGlobalIdGetRequest : HttpRequestMessage<ApplicantGlobalIdResponse>
+    {
+        public ApplicantsGlobalIdGetRequest(int globalId)
+            : base(HttpMethod.Get, "Api/Applicant/GlobalId/" + globalId)
+        {
+        }
+    }
+}
