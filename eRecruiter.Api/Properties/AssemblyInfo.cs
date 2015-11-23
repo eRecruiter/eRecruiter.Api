4da8793d47f79b8fc951f0fea04b08e298fdb8da
Christoph Kopf <christoph.kopf@erecruiter.net>
2015-11-23 13:14:25 +0100

    Minor version upgrade

--- eRecruiter.Api/Properties/AssemblyInfo.cs
+++ eRecruiter.Api/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.6.2.0")]
-[assembly: AssemblyFileVersion("1.6.2.0")]
-[assembly: AssemblyInformationalVersion("1.6.2")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
