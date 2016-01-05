5a8f130d9f96465fafc69e55b599c1fac6569394
Bernhard Wolkerstorfer <bernhard.wolkerstorfer@erecruiter.net>
2016-01-05 16:41:22 +0100

    #26 added requests for sending double-opt-in e-mails

---
+++ eRecruiter.Api/Parameters/Mailer/DoubleOptInParameter.cs
@@ -0,0 +1,31 @@
+﻿namespace eRecruiter.Api.Parameters
+{
+    public class DoubleOptInParameter
+    {
+        /// <summary>
+        /// The applicant's e-mail address.
+        /// </summary>
+        public string EMail { get; set; }
+
+        /// <summary>
+        /// The applicant's firstname.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The applicant's last name.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The double-opt-in verification token for the applicant.
+        /// </summary>
+        public string DoubleOptInToken { get; set; }
+
+        /// <summary>
+        /// The current applicant portal URL (if applicable). This is used to match for applicant portal settings. 
+        /// If empty or not match can be found, the default applicant portal settings will be used.
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
