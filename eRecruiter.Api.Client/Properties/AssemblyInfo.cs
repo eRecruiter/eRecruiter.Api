ee2e1d0891e01ffd051244fb85d37e242cfe6ba4
Bernhard Wolkerstorfer <bernhard.wolkerstorfer@erecruiter.net>
2016-01-05 16:43:28 +0100

    #26 updated api client metadata

--- eRecruiter.Api.Client/Properties/AssemblyInfo.cs
+++ eRecruiter.Api.Client/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
-[assembly: AssemblyInformationalVersion("1.2.3")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
