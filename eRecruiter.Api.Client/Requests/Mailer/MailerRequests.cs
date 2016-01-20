c47b32001406a97f97aff0cd59e64bd52ec503d7
Bernhard Wolkerstorfer <bernhard.wolkerstorfer@erecruiter.net>
2016-01-20 06:51:48 +0100

    #23 changed response type for mailer requests

--- eRecruiter.Api.Client/Requests/Mailer/MailerRequests.cs
+++ eRecruiter.Api.Client/Requests/Mailer/MailerRequests.cs
@@ -1,16 +1,17 @@
 ﻿using eRecruiter.Api.Parameters;
+using eRecruiter.Api.Responses;
 using JetBrains.Annotations;
 
 namespace eRecruiter.Api.Client.Requests
 {
-    public class SendRegistrationMailRequest : PostJsonHttpRequestMessage<RegistrationMailParameter>
+    public class SendRegistrationMailRequest : PostJsonHttpRequestMessage<MailerResponse>
     {
         public SendRegistrationMailRequest([NotNull] RegistrationMailParameter parameter) : base("Api/Mailer/Registration", parameter)
         {
         }
     }
 
-    public class SendApplicationMailRequest : PostJsonHttpRequestMessage<ApplicationMailParameter>
+    public class SendApplicationMailRequest : PostJsonHttpRequestMessage<MailerResponse>
     {
         public SendApplicationMailRequest([NotNull] RegistrationMailParameter parameter) : base("Api/Mailer/Application", parameter)
         {
