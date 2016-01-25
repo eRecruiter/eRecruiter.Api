d8d2e31e3c279d57f8c25e326b2c02376fbf0c2d
Bernhard Wolkerstorfer <bernhard.wolkerstorfer@erecruiter.net>
2016-01-25 12:46:46 +0100

    #23 fixed typos in comments

--- eRecruiter.Api/Parameters/Mailer/ApplicationMailParameter.cs
+++ eRecruiter.Api/Parameters/Mailer/ApplicationMailParameter.cs
@@ -9,7 +9,7 @@
 
         /// <summary>
         /// The current applicant portal URL (if applicable). This is used to match for applicant portal settings. 
-        /// If empty or not match can be found, the default applicant portal settings will be used.
+        /// If empty or no match can be found, the default applicant portal settings will be used.
         /// </summary>
         public string Url { get; set; }
     }
