3ccfcba0b29990055c8ab166794306ce7a95bfb0
Christoph Kopf <christoph.kopf@erecruiter.net>
2016-02-17 14:36:10 +0100

    Add CompanyImportResponse

---
+++ eRecruiter.Api/Responses/Import/CompanyImportResponse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace eRecruiter.Api.Responses
+{
+    public class CompanyImportResponse
+    {
+        public SyncResult Result { get; set; }
+    }
+
+    public class SyncResult
+    {
+        public string Identifiyer { get; set; }
+        public string Type { get; set; }
+        public IEnumerable<Message> Messages { get; set; }
+        public IEnumerable<SyncResult> NestedResults { get; set; }
+    }
+
+    public class Message
+    {
+        public string Text { get; set; }
+        public Severity Severity { get; set; }
+    }
+
+    public enum Severity
+    {
+        Trace,
+        Debug,
+        Info,
+        Warning,
+        Error
+    }
+}
