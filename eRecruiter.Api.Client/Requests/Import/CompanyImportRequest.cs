3504678e11959d6684fe45d30fb573cfe4d4867b
Christoph Kopf <christoph.kopf@erecruiter.net>
2016-02-17 14:38:27 +0100

    Add CompanyImportRequest with XML as plain text in http body

---
+++ eRecruiter.Api.Client/Requests/Import/CompanyImportRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Text;
+using eRecruiter.Api.Responses;
+
+namespace eRecruiter.Api.Client.Requests
+{
+    public class CompanyImportRequest :  HttpRequestMessage<CompanyImportResponse>
+    {
+        public CompanyImportRequest(string inputXml, bool isTest) : base(HttpMethod.Put, "Api/Import/Company?isTest=" + isTest)
+        {
+            Content = new StringContent(inputXml, Encoding.UTF8);
+        }
+    }
+}
