25dfcb8472890d8846fb0d2741d756bdf8e8f00a
Christoph Kopf <christoph.kopf@erecruiter.net>
2016-02-18 15:07:16 +0100

    Print Status

--- eRecruiter.Api.CommandLineClient/CompanyImportRequest.cs
+++ eRecruiter.Api.CommandLineClient/CompanyImportRequest.cs
@@ -13,15 +13,28 @@ namespace eRecruiter.Api.CommandLineClient
     {
         public static async Task Run(ApiHttpClient client, string xmlFileLocation, bool isValidationOnly)
         {
+            // Build Body
             var xmlString = System.IO.File.ReadAllText(xmlFileLocation);
             var bodyParmeter = new CompanyImportParameter
             {
                 XMLContent = Encoding.UTF8.GetBytes(xmlString)
             };
 
+            // Request and await response
             var companyImportResponse = await new Client.Requests.CompanyImportRequest(isValidationOnly, bodyParmeter).LoadResultAsync(client);
             
+            // Print response result
             Console.WriteLine(RecursiveResultStringBuilder(companyImportResponse.Result));
+
+            // Print response stati
+            Console.WriteLine("========== Stati ==========");
+            var stati = SplitEnumFlags<StatusType>(companyImportResponse.Status);
+            stati.ToList().ForEach(x => Console.WriteLine(x));
+        }
+
+        private static IEnumerable<T> SplitEnumFlags<T>(Enum mask)
+        {
+            return Enum.GetValues(typeof(T)).Cast<Enum>().Where(mask.HasFlag).Cast<T>().ToList();
         }
 
         private static string RecursiveResultStringBuilder(SyncResult result)
@@ -29,7 +42,6 @@ namespace eRecruiter.Api.CommandLineClient
             var sb = new StringBuilder();
             sb.AppendLine("============================================================================");
             sb.AppendLine(string.Format("{0,-30}{1, -30}", "Type", "Identifiyer"));
-            sb.AppendLine("----------------------------------------------------------------------------");
             sb.AppendLine(string.Format("{0,-30}{1, -30}", result.Type, result.Identifiyer));
 
             var i = 0;
@@ -37,12 +49,12 @@ namespace eRecruiter.Api.CommandLineClient
             {
                 sb.AppendLine(string.Format("{0,4} [{1}] {2}", "#" + i++, message.Severity, message.Text));
             }
-            
+
             foreach (var nestedResult in result.NestedResults)
             {
                 sb.AppendLine(RecursiveResultStringBuilder(nestedResult));
             }
-            
+
             return sb.ToString();
         }
     }
