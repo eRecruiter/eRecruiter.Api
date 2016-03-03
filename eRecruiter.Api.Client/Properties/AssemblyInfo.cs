68889069ca69974cc92c9290d3faab5ea5c91ff5
Christoph Kopf <christoph.kopf@erecruiter.net>
2016-03-03 13:27:11 +0100

    Add remaining version number properties

--- eRecruiter.Api.Client/Properties/AssemblyInfo.cs
+++ eRecruiter.Api.Client/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@
 
 [assembly: AssemblyVersion("1.6.0.0")]
 [assembly: AssemblyFileVersion("1.6.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
--- eRecruiter.Api/Properties/AssemblyInfo.cs
+++ eRecruiter.Api/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@
 
 [assembly: AssemblyVersion("1.13.0.0")]
 [assembly: AssemblyFileVersion("1.13.0.0")]
-[assembly: AssemblyInformationalVersion("1.12.0")]
+[assembly: AssemblyInformationalVersion("1.13.0")]
