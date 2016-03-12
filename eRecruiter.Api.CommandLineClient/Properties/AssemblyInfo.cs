fcf23a1e32249d10701c9a75a2a57a8bf8830a46
Bernhard Wolkerstorfer <bernhard.wolkerstorfer@erecruiter.net>
2016-03-12 11:32:24 +0100

    fixed CLI client to allow API invocation

---
+++ eRecruiter.Api.CommandLineClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("eRecruiter.Api.CommandLineClient")]
+[assembly: AssemblyDescription("eRecruiter API CLI Client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ePunkt Internet Recruiting GmbH")]
+[assembly: AssemblyProduct("eRecruiter")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
