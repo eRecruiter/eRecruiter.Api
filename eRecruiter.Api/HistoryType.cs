aa5b03d8c9404d78cbc6288db5d538a99c12398f
Adrian Fürschuß <adrian.fuerschuss@epunkt.net>
2016-04-06 13:28:51 +0200

    #37 Add parameter and response objects for history entry.

---
+++ eRecruiter.Api/HistoryType.cs
@@ -0,0 +1,17 @@
+﻿namespace eRecruiter.Api
+{
+    /// <summary>
+    /// The poosible types of a history entry.
+    /// </summary>
+    public enum HistoryType
+    {
+        /// <summary>
+        /// A manually (mostly by a user) created history entry.
+        /// </summary>
+        Manual,
+        /// <summary>
+        /// The history entry was created during an import.
+        /// </summary>
+        Import
+    }
+}
