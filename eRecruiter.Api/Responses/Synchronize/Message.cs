06987458579c362108de355d321af566580e9c5d
Christoph Kopf <christoph.kopf@erecruiter.net>
2016-07-04 13:57:36 +0200

    #39 Split classes

---
+++ eRecruiter.Api/Responses/Synchronize/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace eRecruiter.Api.Responses
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public Severity Severity { get; set; }
+    }
+}
---
+++ eRecruiter.Api/Responses/Synchronize/StatusType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace eRecruiter.Api.Responses
+{
+    [Flags]
+    public enum StatusType
+    {
+        ValidData = 1,
+        InValidData = 2,
+        PeristingSkiped = 4,
+        PersistingError = 8,
+        PersistingSuccess = 16
+        // = 32
+        // = 64
+        // = ...
+    }
+}
