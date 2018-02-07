9c794831f71bed8fb022b0486cbb15c30a309740
Bernhard Wolkerstorfer <bernhard.wolkerstorfer@erecruiter.net>
2018-02-07 15:03:38 +0100

    [DOD-2062] added request/response to validate passwords against the configured policy

---
+++ eRecruiter.Api.Client/Requests/Account/ValidatePasswordRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using eRecruiter.Api.Responses;
+
+namespace eRecruiter.Api.Client.Requests
+{
+    /// <summary>
+    /// Request to validate a password against the password policy.
+    /// </summary>
+    public class ValidatePasswordRequest : HttpRequestMessage<ValidatePasswordResponse>
+    {
+        public ValidatePasswordRequest(string passwordToValidate) 
+            : base(HttpMethod.Get, $"Api/Validation/Password?password={passwordToValidate}")
+        {
+        }
+    }
+}
