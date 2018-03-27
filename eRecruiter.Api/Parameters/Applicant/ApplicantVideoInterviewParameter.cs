33cb27fbd70a83cad030f008eb485a72fd1a209e
Carlos Lopes <carlos.lopes@findmore.eu>
2018-03-27 13:13:42 +0100

    [PDE-150] - video recruiting interview api - type correction

--- eRecruiter.Api/Parameters/Applicant/ApplicantVideoInterviewParameter.cs
+++ eRecruiter.Api/Parameters/Applicant/ApplicantVideoInterviewParameter.cs
@@ -19,11 +19,11 @@ namespace eRecruiter.Api.Parameters
         /// <summary>
         /// Template external id from the video recruiter provider
         /// </summary>
-        public string TemplateId { get; set; }
+        public int TemplateId { get; set; }
         /// <summary>
         /// Interview external id from the video recruiter provider
         /// </summary>
-        public string InterviewId { get; set; }
+        public int InterviewId { get; set; }
         /// <summary>
         /// Date of interview
         /// </summary>
