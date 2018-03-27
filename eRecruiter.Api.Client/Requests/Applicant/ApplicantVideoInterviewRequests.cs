2d5fd7dcb2b46f4a86ad4a42b3ec2950336a0c35
Carlos Lopes <carlos.lopes@findmore.eu>
2018-03-27 13:04:13 +0100

    [PDE-150] - video recruiting interview api - new object and call

--- eRecruiter.Api.Client/Requests/Applicant/ApplicantVideoInterviewRequests.cs
+++ eRecruiter.Api.Client/Requests/Applicant/ApplicantVideoInterviewRequests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
+using eRecruiter.Api.Parameters;
 using eRecruiter.Api.Responses;
 
 namespace eRecruiter.Api.Client.Requests
@@ -15,6 +16,22 @@ namespace eRecruiter.Api.Client.Requests
         /// <param name="applicantId"></param>
         public ApplicantVideoInterviewRequests(int applicantId) 
             : base(HttpMethod.Get, $"/Api/Applicant/{applicantId}/VideoInterviews")
+        {
+        }
+    }
+
+    /// <summary>
+    /// Creates video interview.
+    /// </summary>
+    public class ApplicantVideoInterviewPutRequest : PutJsonHttpRequestMessage<ApplicantVideoInterviewResponse>
+    {
+        /// <summary>
+        /// Creates video interview for applicant.
+        /// </summary>
+        /// <param name="applicantId"></param>
+        /// <param name="applicantVideoInterviewParameter"></param>
+        public ApplicantVideoInterviewPutRequest(int applicantId, ApplicantVideoInterviewParameter applicantVideoInterviewParameter)
+            : base($"/Api/Applicant/{applicantId}/VideoInterviews", applicantVideoInterviewParameter)
         {
         }
     }
---
+++ eRecruiter.Api/Parameters/Applicant/ApplicantVideoInterviewParameter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace eRecruiter.Api.Parameters
+{
+    public class ApplicantVideoInterviewParameter
+    {
+        /// <summary>
+        /// Id of the erecruiter job
+        /// </summary>
+        public int JobId { get; set; }
+        /// <summary>
+        /// Id of the erecruiter application
+        /// </summary>
+        public int ApplicationId { get; set; }
+        /// <summary>
+        /// Id of the video recruiter provider
+        /// </summary>
+        public VideoRecruitingProvider Provider { get; set; }
+        /// <summary>
+        /// Template external id from the video recruiter provider
+        /// </summary>
+        public string TemplateId { get; set; }
+        /// <summary>
+        /// Interview external id from the video recruiter provider
+        /// </summary>
+        public string InterviewId { get; set; }
+        /// <summary>
+        /// Date of interview
+        /// </summary>
+        public DateTime? InterviewDate { get; set; }
+    }
+}
