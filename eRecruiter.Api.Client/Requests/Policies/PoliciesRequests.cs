f698c8f392af6325f59de3cd2479046d3caa7a8f
Bernhard Wolkerstorfer <bwolkerstorfer@users.noreply.github.com>
2018-05-25 11:28:23 +0200

    Update PoliciesRequests.cs
    added missing equals

--- eRecruiter.Api.Client/Requests/Policies/PoliciesRequests.cs
+++ eRecruiter.Api.Client/Requests/Policies/PoliciesRequests.cs
@@ -16,7 +16,7 @@ namespace eRecruiter.Api.Client.Requests
         /// <param name="portal">The portal.</param>
         /// <param name="culture">The culture.</param>
         public PoliciesGetRequest(string portal, CultureInfo culture)
-            : base(HttpMethod.Get, "Api/Policies?portal" + portal + "&culture=" + culture.Name)
+            : base(HttpMethod.Get, "Api/Policies?portal=" + portal + "&culture=" + culture.Name)
         {
         }
         public PoliciesGetRequest(string portal) : this(portal, CultureInfo.CurrentCulture) { }
