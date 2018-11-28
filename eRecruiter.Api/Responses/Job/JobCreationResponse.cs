14642f2b934feed402e68b77e437c921db085768
Leonardo Oliveira <leonardo.oliveira@findmore.pt>
2018-11-28 11:03:05 +0000

    [PDE-296] api job creation - Removed unnecessary properties

--- eRecruiter.Api/Responses/Job/JobCreationResponse.cs
+++ eRecruiter.Api/Responses/Job/JobCreationResponse.cs
@@ -5,7 +5,5 @@ namespace eRecruiter.Api.Responses
     public class JobCreationResponse
     {
         public int Id { get; set; }
-        public HttpStatusCode StatusCode { get; set; }
-        public string Message { get; set; }
     }
 }
