f16961321a28d0b23e7d9158f52992ca7d882eb6
duartejbrito <sytertzp@gmail.com>
2019-05-08 10:24:16 +0100

    [PDE-415]: Added new endpoint to create a new applicant and application for the data from "Apply by karriere.at". - `KarriereOneClickResponse` has been added. - `KarriereOneClickRequest` has been added.

---
+++ eRecruiter.Api.Client/Requests/Integrations/KarriereOneClickRequest.cs
@@ -0,0 +1,18 @@
+﻿using eRecruiter.Api.Responses.Integrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRecruiter.Api.Client.Requests.Integrations
+{
+    public class KarriereOneClickRequest : HttpRequestMessage<KarriereOneClickResponse>
+    {
+        public KarriereOneClickRequest()
+            : base(HttpMethod.Post, "Api/Integrations/KarriereOneClick")
+        {
+        }
+    }
+}
