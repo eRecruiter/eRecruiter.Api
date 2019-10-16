21c8b4885cb335dca6f10637c66c7890188fd8f1
duartejbrito <sytertzp@gmail.com>
2019-10-16 11:22:38 +0100

    [PDE-725]: Change `AskForVideoPitchOnRegistration` to `DisplayType` on `PortalSettingsResponse`

--- eRecruiter.Api/Properties/AssemblyInfo.cs
+++ eRecruiter.Api/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.68.1.0")]
-[assembly: AssemblyFileVersion("1.68.1.0")]
-[assembly: AssemblyInformationalVersion("1.68.1")]
+[assembly: AssemblyVersion("1.68.2.0")]
+[assembly: AssemblyFileVersion("1.68.2.0")]
+[assembly: AssemblyInformationalVersion("1.68.2")]
