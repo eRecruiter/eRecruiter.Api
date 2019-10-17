fc367f3cf38bb307b74d55c30737205564976187
duartejbrito <sytertzp@gmail.com>
2019-10-17 14:33:33 +0100

    [PDE-725]: Fix `ApplicantVideoInterviewPutRequest` to return `IEnumerable<ApplicantVideoInterviewResponse>`

--- eRecruiter.Api.Client/Properties/AssemblyInfo.cs
+++ eRecruiter.Api.Client/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.45.2.0")]
-[assembly: AssemblyFileVersion("1.45.2.0")]
-[assembly: AssemblyInformationalVersion("1.45.2")]
+[assembly: AssemblyVersion("1.45.3.0")]
+[assembly: AssemblyFileVersion("1.45.3.0")]
+[assembly: AssemblyInformationalVersion("1.45.3")]
