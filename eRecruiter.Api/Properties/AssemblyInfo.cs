1e071058660e4dbf9d78baf8ab440602f3e64475
duartejbrito <sytertzp@gmail.com>
2020-08-24 14:31:12 +0100

    [PDE-1279]: Add `HiringOrganization` to `GoogleForJobsResponse`

--- eRecruiter.Api/Properties/AssemblyInfo.cs
+++ eRecruiter.Api/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.76.0.0")]
-[assembly: AssemblyFileVersion("1.76.0.0")]
-[assembly: AssemblyInformationalVersion("1.76.0")]
+[assembly: AssemblyVersion("1.77.0.0")]
+[assembly: AssemblyFileVersion("1.77.0.0")]
+[assembly: AssemblyInformationalVersion("1.77.0")]
