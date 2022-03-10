44091105a568d001a2448d463d82127ceccfb397
Ivan Šola <Ivan.Sola@erecruiter.net>
2022-03-10 14:55:04 +0100

    [PDE-2660] Adjust request naming to fit convention

--- eRecruiter.Api.Client/Properties/AssemblyInfo.cs
+++ eRecruiter.Api.Client/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.54.2.0")]
-[assembly: AssemblyFileVersion("1.54.2.0")]
-[assembly: AssemblyInformationalVersion("1.54.2")]
+[assembly: AssemblyVersion("1.54.3.0")]
+[assembly: AssemblyFileVersion("1.54.3.0")]
+[assembly: AssemblyInformationalVersion("1.54.3")]
